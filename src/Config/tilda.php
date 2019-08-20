8e5bde701b98e6d065d8c454cc61e38ebd2fcb0e
Michael Ledin <mledin89@gmail.com>
2019-08-20 17:49:46 +0300

    v0.0.7
    tilda-php update.

--- src/Config/tilda.php
+++ src/Config/tilda.php
@@ -6,7 +6,9 @@ return [
         'publicKey' => env('TILDA_API_PUBLIC_KEY'),
         'secretKey' => env('TILDA_API_SECRET_KEY'),
     ],
-    'htmlFileName' => env('TILDA_HTML_FILE_NAME'),
     'path' => env('TILDA_PATH'),
+    'cssPath' => env('TILDA_CSS_PATH'),
+    'jsPath' => env('TILDA_JS_PATH'),
+    'imagesPath' => env('TILDA_IMAGES_PATH'),
     'projects' => explode(',', env('TILDA_PROJECTS', '')),
 ];
